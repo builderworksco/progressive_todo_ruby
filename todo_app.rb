2fdb98866a79202ad48131b8703018c240b4387d
Justin Reese <justin.x.reese@gmail.com>
2015-08-12 00:19:39 -0400

    Stores todo list as an array

--- todo_app.rb
+++ todo_app.rb
@@ -1,11 +1,6 @@
-list = "1.) Buy Milk \n"+
-  "2.) Buy Cheese \n"+
-  "3.) Learn Ruby \n"+
-  "4.) Feed the cats \n"+
-  "5.) Register for conference (conference) \n"+
-  "6.) Book hotel (conference) \n"+
-  "7.) Book flight (conference) \n"+
-  "8.) Rent a car (conference) \n"+
-  "9.) Research ninjitsu"
+list = ["1.) Buy Milk \n", "2.) Buy Cheese \n", "3.) Learn Ruby \n",
+  "4.) Feed the cats \n", "5.) Register for conference (conference) \n",
+  "6.) Book hotel (conference) \n", "7.) Book flight (conference) \n",
+  "8.) Rent a car (conference) \n", "9.) Research ninjitsu"]
 
 puts list
