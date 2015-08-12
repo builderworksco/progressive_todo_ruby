6bf1da1e355873693e053c1b2e293d3ecd42d922
Justin Reese <justin.x.reese@gmail.com>
2015-08-12 00:27:17 -0400

    Interpolates list numbers and output formating

--- todo_app.rb
+++ todo_app.rb
@@ -1,9 +1,9 @@
-list = [".) Buy Milk", ".) Buy Cheese", ".) Learn Ruby",
-  ".) Feed the cats", ".) Register for conference (conference)",
-  ".) Book hotel (conference)", ".) Book flight (conference)",
-  ".) Rent a car (conference)", ".) Research ninjitsu"]
+list = ["Buy Milk", "Buy Cheese", "Learn Ruby",
+  "Feed the cats", "Register for conference (conference)",
+  "Book hotel (conference)", "Book flight (conference)",
+  "Rent a car (conference)", "Research ninjitsu"]
 
 list.each_with_index do |list_item, index|
   number = index + 1
-  puts number.to_s + list_item
+  puts "#{number}.) #{list_item}"
 end
