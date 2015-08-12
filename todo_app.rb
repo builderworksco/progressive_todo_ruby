a4dd8c9828b1e83b25bffb792747770aed13d3e4
Justin Reese <justin.x.reese@gmail.com>
2015-08-12 00:34:34 -0400

    Adds a bunch of demonstrative comments

--- todo_app.rb
+++ todo_app.rb
@@ -1,3 +1,19 @@
+=begin
+
+todo_app.rb
+
+This awesome app is meant to demonstrate how to code in Ruby.
+Each branch of this app is progressively advanced.
+
+::::::::::::   ...    :::::::-.      ...
+;;;;;;;;''''.;;;;;;;.  ;;,   `';, .;;;;;;;.
+     [[    ,[[     \[[,`[[     [[,[[     \[[,
+     $$    $$$,     $$$ $$,    $$$$$,     $$$
+     88,   "888,_ _,88P 888_,o8P'"888,_ _,88P
+     MMM     "YMMMMMP"  MMMMP"`    "YMMMMMP"
+=end
+
+# List of things to do
 list = ["Buy Milk", "Buy Cheese", "Learn Ruby",
   "Feed the cats", "Register for conference (conference)",
   "Book hotel (conference)", "Book flight (conference)",
@@ -5,5 +21,5 @@ list = ["Buy Milk", "Buy Cheese", "Learn Ruby",
 
 list.each_with_index do |list_item, index|
   number = index + 1
-  puts "#{number}.) #{list_item}"
+  puts "#{number}.) #{list_item}" # looks like "1.) List item description"
 end
