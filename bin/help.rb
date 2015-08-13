248209eaf41b85df441cd4cc6e3262087cba7669
Justin Reese <justin.x.reese@gmail.com>
2015-08-12 23:48:14 -0400

    Adds new TodoListManual that is also printable

---
+++ bin/help.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/models/todo_list_manual'
+
+manual = TodoListManual.new
+manual.print
---
+++ lib/models/todo_list_manual.rb
@@ -0,0 +1,10 @@
+require_relative 'printable'
+class TodoListManual < Printable
+  def title
+    'Todo List'
+  end
+
+  def body
+    'This is the manual for the Todo list app'
+  end
+end
