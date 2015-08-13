f9f00ce259ac0305ed06c86c9939a97e1d6a689b
Justin Reese <justin.x.reese@gmail.com>
2015-08-13 01:03:53 -0400

    Passes an empty hash as default TodoItem start point

--- lib/models/todo_item.rb
+++ lib/models/todo_item.rb
@@ -1,7 +1,7 @@
 class TodoItem
   attr_accessor :text, :is_complete, :order
 
-  def initialize(options)
+  def initialize(options = {})
     @text = options[:text]
     @is_complete = options[:is_complete]
     @order = options[:order]
