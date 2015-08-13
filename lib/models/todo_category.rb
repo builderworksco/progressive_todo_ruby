7f942e713407f98e5f0c8e0fecf4855629e9324b
Justin Reese <justin.x.reese@gmail.com>
2015-08-13 01:19:11 -0400

    Represents TodoCategories with a class

---
+++ lib/models/todo_category.rb
@@ -0,0 +1,25 @@
+require_relative 'printable'
+
+class TodoCategory < Printable
+  attr_accessor :name, :items_input, :items
+
+  def initialize(name, *items_input)
+    @name = name
+    @items_input = items_input
+    @items = []
+    format_input
+  end
+
+  def format_input
+    item_order = 1
+    @items_input.each do |text|
+      @items << TodoItem.new(text: text, order: item_order)
+      item_order += 1
+    end
+  end
+
+  def print
+    print_as_title(@name)
+    @items.map(&:print)
+  end
+end
--- lib/models/todo_list.rb
+++ lib/models/todo_list.rb
@@ -1,42 +1,20 @@
 require_relative 'printable'
 require_relative 'todo_item'
+require_relative 'todo_category'
 
 class TodoList < Printable
   def initialize
     @title = 'My TODO list'
-    @list = generate_list
   end
 
-  def generate_list
-    general = []
-    item_order = 1
-    ["Buy Milk", "Buy Cheese", "Learn Ruby", "Feed the cats", "Research ninjitsu"].each do |text|
-      general << TodoItem.new(text: text, order: item_order)
-      item_order += 1
-    end
-
-    conference = []
-    item_order = 1
-    ["Register for conference", "Book hotel", "Book flight", "Rent a car"].each do |text|
-      conference << TodoItem.new(text: text, order: item_order)
-      item_order += 1
-    end
-
-    {
-      general: general,
-      conference: conference
-    }
+  def list
+    [
+      TodoCategory.new(:general, "Buy Milk", "Buy Cheese", "Learn Ruby", "Feed the cats", "Research ninjitsu"),
+      TodoCategory.new(:conference, "Register for conference", "Book hotel", "Book flight", "Rent a car")
+    ]
   end
 
   def print_body
-    print_list
-  end
-
-  def print_list
-    @list.each do |list_name, list|
-      print_as_title(list_name)
-      list.map(&:print)
-      puts "\n"
-    end
+    list.map(&:print)
   end
 end
